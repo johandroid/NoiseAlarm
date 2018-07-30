ce6b43085acc3cdee0d5b95b38be1ea4392553a4
Johan Alexis Duque Cadena <johan.duque@gmail.com>
2018-07-30 23:57:56 +0200

    Measuring thresholds for concept proof, now it's necessary to add some AI stuff

--- NoiseAlarm_Arduino_Code/NoiseAlarm_Arduino_Code.ino
+++ NoiseAlarm_Arduino_Code/NoiseAlarm_Arduino_Code.ino
@@ -1,60 +1,35 @@
-#include <TM1637Display.h>
-
-const int MIC = 0; //the microphone amplifier output is connected to pin A0
-const int MIC_DO = 11; 
-const int LED = 10; 
-const int CLK = 9; //Set the CLK pin connection to the display
-const int DIO = 8; //Set the DIO pin connection to the display
-const int buzzerPin = 3; //Define buzzerPin
-const int COUNTER_VAL = 10; 
-
-int adc;
-int dB, PdB; //the variable that will hold the value read from the microphone each time
-int counter = 0; 
-
-TM1637Display display(CLK, DIO);  //set up the 4-Digit Display.
-
-
-void beep(unsigned char delayms) { //creating function
-  analogWrite(buzzerPin, 1); //Setting pin to high
-  delay(delayms); //Delaying
-  analogWrite(buzzerPin ,0); //Setting pin to LOW
-  delay(delayms); //Delaying
-  
+#define MicSamples (1024*2)
+#define MicPin A0
+ 
+void MeasureAnalog()
+{
+    long signalAvg = 0, signalMax = 0, signalMin = 1024, t0 = millis();
+    for (int i = 0; i < MicSamples; i++)
+    {
+        int k = analogRead(MicPin);
+        signalMin = min(signalMin, k);
+        signalMax = max(signalMax, k);
+        signalAvg += k;
+    }
+    signalAvg /= MicSamples;
+ 
+    /*Serial.print("Time: " + String(millis() - t0));
+    Serial.print(" Min: " + String(signalMin));
+    Serial.print(" Max: " + String(signalMax));
+    Serial.print(" Span: " + String(signalMax - signalMin));
+    Serial.print(", " + String(signalMax - signalAvg));
+    Serial.print(", " + String(signalAvg - signalMin));*/
+    Serial.print(" Avg: " + String(signalAvg));
+    Serial.println("");
 }
 
 void setup() {
-  display.setBrightness(0x00);  //set the diplay to minimum brightness
-  pinMode(LED, OUTPUT);
-  pinMode(MIC_DO, INPUT);
+  // put your setup code here, to run once:
+  Serial.begin(9600);
 }
 
-void printDataScreen()
-{
-  if(counter <= 0)
-  {
-    int roundedVal = (dB/10.0) * 100; 
-    display.showNumberDecEx(roundedVal,0b00100000); //Display the Variable value;
-    //display.showNumberDecEx(adc);
-    counter = COUNTER_VAL; 
-  }else{
-    counter--; 
-  }
+void loop() {
+  MeasureAnalog(); 
+  // put your main code here, to run repeatedly:
+
 }
-void loop(){
-
-  PdB = dB; //Store the previous of dB here
-  adc= analogRead(MIC); //Read the ADC value from amplifer 
-  dB = (adc+83.2073) / 11.003; //Convert ADC value to dB using Regression values
-
-  if(digitalRead(MIC_DO) == HIGH)
-  {
-    digitalWrite(LED, HIGH); 
-    delay(1);  
-    digitalWrite(LED, LOW); 
-  }
-  
-  printDataScreen();  
-}
-
-
